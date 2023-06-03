59709c5a4ef85217549e4b9ed89225cc687ba3dc
Saúl Sánchez Rangel <a01383954@tec.mx>
2023-06-02 19:24:58 -0600

    DB structure & storedprocedures for api.ts

---
+++ BarryDB.sql
@@ -0,0 +1,47 @@
+-- drop all tables in BarryDB
+drop table Certificado;
+drop table Minijuego;
+drop table Estudiante;
+drop table Maestro;
+drop table CatalogoMinijuegos;
+
+CREATE TABLE Estudiante(
+Matricula CHAR(9) PRIMARY KEY NOT NULL, 
+Nombre VARCHAR(40) NOT NULL,
+ApPaterno VARCHAR(25), 
+ApMaterno VARCHAR(25),
+Correo VARCHAR(60),
+CodigoAcceso VARCHAR (15),
+Progreso INT,
+Estatus BIT
+);
+
+CREATE TABLE CatalogoMinijuegos(
+Id INT PRIMARY KEY NOT NULL,
+Nombre VARCHAR(20) NOT NULL,
+Descripcion VARCHAR(100)
+);
+
+CREATE TABLE Minijuego(
+Id INT IDENTITY PRIMARY KEY NOT NULL,
+Matricula CHAR(9) FOREIGN KEY REFERENCES Estudiante(Matricula),
+IdVideoJuego INT FOREIGN KEY REFERENCES CatalogoMinijuegos(Id),
+Completado BIT
+);
+
+CREATE TABLE Maestro(
+Matricula CHAR(9) PRIMARY KEY NOT NULL,
+Nombre  VARCHAR(40) NOT NULL,
+ApPaterno VARCHAR(25),
+ApMaterno VARCHAR(25),
+Correo VARCHAR(60),
+CodigoAcceso VARCHAR (15)
+);
+
+CREATE TABLE Certificado(
+NumeroSerie CHAR(10) PRIMARY KEY NOT NULL,
+Matricula CHAR(9) FOREIGN KEY REFERENCES Estudiante(Matricula),
+MaestroAutorizo CHAR(9) FOREIGN KEY REFERENCES Maestro(Matricula),
+FechaVencimiento DATETIME,
+FechaEmision DATETIME
+);
---
+++ Procedures.sql
@@ -0,0 +1,124 @@
+CREATE PROCEDURE ObtenerTodosLosEstudiantes
+AS
+BEGIN
+    SELECT *
+    FROM Estudiante
+END
+GO
+
+CREATE PROCEDURE ObtenerCertificados
+@Matricula INT
+AS
+BEGIN
+    SELECT *
+    FROM Certificado
+    WHERE Matricula = @Matricula
+END
+GO
+
+CREATE PROCEDURE ObtenerEstudiante
+@Matricula CHAR(9)
+AS
+BEGIN
+    SELECT *
+    FROM Estudiante
+    WHERE Matricula = @Matricula
+END
+GO
+
+CREATE PROCEDURE ObtenerMinijuegosPorEstudiante
+@Matricula CHAR(9)
+AS
+BEGIN
+    SELECT CatalogoMinijuegos.Nombre, Minijuego.Completado
+    FROM Minijuego
+             JOIN CatalogoMinijuegos ON Minijuego.IdVideoJuego = CatalogoMinijuegos.Id
+    WHERE Minijuego.Matricula = @Matricula
+END
+GO
+
+CREATE PROCEDURE CrearCertificado
+    @Matricula CHAR(9)
+AS
+BEGIN
+    DECLARE @NombreEstudiante VARCHAR(100)
+    DECLARE @FechaEmision DATE
+    DECLARE @NumeroSerie CHAR(10)
+
+    -- Get the name of the student
+    SELECT @NombreEstudiante = CONCAT(Nombre, ' ', ApPaterno, ' ', ApMaterno)
+    FROM Estudiante
+    WHERE Matricula = @Matricula
+
+    -- Set the date of issuance to today's date
+    SET @FechaEmision = GETDATE()
+
+    -- Generate a unique certificate number
+    SET @NumeroSerie = CONCAT('CERT-', @Matricula, '-', CONVERT(VARCHAR(8), GETDATE(), 112))
+
+    -- Insert the certificate into the Certificado table
+    INSERT INTO Certificado (NumeroSerie, Matricula, FechaEmision)
+    VALUES (@NumeroSerie, @Matricula, @FechaEmision)
+END
+GO
+
+CREATE PROCEDURE ActualizarEstatusEstudiante
+    @Matricula CHAR(9),
+    @NuevoEstatus BIT
+AS
+BEGIN
+    UPDATE Estudiante
+    SET Estatus = @NuevoEstatus
+    WHERE Matricula = @Matricula
+END
+GO
+
+CREATE PROCEDURE ActualizarProgresoEstudiante
+    @Matricula CHAR(9),
+    @NuevoProgreso INT
+AS
+BEGIN
+    UPDATE Estudiante
+    SET Progreso = @NuevoProgreso
+    WHERE Matricula = @Matricula
+END
+GO
+
+CREATE PROCEDURE UpdateMinijuegoState
+    @MinijuegoId INT,
+    @AlumnoMatricula CHAR(9),
+    @NuevoEstado BIT
+AS
+BEGIN
+    UPDATE Minijuego
+    SET Completado = @NuevoEstado
+    WHERE IdVideoJuego = @MinijuegoId AND Matricula = @AlumnoMatricula
+END
+GO
+
+CREATE PROCEDURE CreateRegistrosForUser
+    @Matricula CHAR(9)
+AS
+BEGIN
+    DECLARE @CatalogoMinijuegosId INT
+    DECLARE @MinijuegoId INT
+
+    -- Get the Id of the CatalogoMinijuegos
+    SELECT @CatalogoMinijuegosId = Id FROM CatalogoMinijuegos
+
+    -- Insert a new row in Minijuego for each CatalogoMinijuegos
+    INSERT INTO Minijuego (Matricula, IdVideoJuego, Completado)
+    SELECT @Matricula, Id, 0 FROM CatalogoMinijuegos
+
+END
+GO
+
+CREATE PROCEDURE CredencialesProfesor
+    @Correo VARCHAR(60),
+    @CodigoAcceso VARCHAR(15)
+AS
+BEGIN
+    SELECT *
+    FROM Maestro
+    WHERE Correo = @Correo AND CodigoAcceso = @CodigoAcceso
+END
