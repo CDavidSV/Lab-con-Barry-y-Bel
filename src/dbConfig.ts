2088629509882b7ed06d6093d50e705a6acfc1e4
Saúl Sánchez Rangel <a01383954@tec.mx>
2023-06-06 15:43:56 -0600

    connecting to azure server

--- src/dbConfig.ts
+++ src/dbConfig.ts
@@ -1,8 +1,8 @@
 // MS SQL Config
 const config = {
-    user: "Viper",
-    password: "1234",
-    server: "VIPER-PC",
+    user: "adminlab",
+    password: "Tecate00.",
+    server: "lab-barry-bel.database.windows.net",
     database: "BarryDB",
     options: {
         trustServerCertificate: true,
@@ -11,7 +11,7 @@ const config = {
         instancename: "",
         encrypt: true 
     },
-    port: 49693
+    port: 1433
 }
 
 export default config;
